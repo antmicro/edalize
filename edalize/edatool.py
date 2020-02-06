e2e1332ef567a5a8e5b8db0407ae06092081e6c5
Karol Gugala <kgugala@antmicro.com>
2020-02-06 15:50:45 +0100

    add possibility to validate arguments by subtools
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- edalize/edatool.py
+++ edalize/edatool.py
@@ -104,9 +104,16 @@ class Edatool(object):
         else:
             logger.warning("Invalid API version '{}' for get_tool_options".format(api_ver))
 
-    def configure(self, args=[]):
-        if args:
-            logger.error("Edalize has stopped supporting passing arguments as a function argument. Set these values as default values in the EDAM object instead")
+    def check_args(self, unknown):
+        # If a tool is using subtools some of the argument may be
+        # parsed by the subtool. This function is used to check if
+        # all the provided arguments were correct. A tool can override
+        # this function to provide custom args checking logic.
+
+        if unknown:
+            raise Exception(f'Unknown command line option {unknown[0]}')
+
+    def configure(self, args):
         logger.info("Setting up project")
         self.configure_pre()
         self.configure_main()
@@ -231,6 +238,7 @@ class Edatool(object):
         backend_members = [x['name'] for x in _opts.get('members', [])]
         backend_lists   = [x['name'] for x in _opts.get('lists', [])]
         known, unknown = parser.parse_known_args(args)
+        self.check_args(unknown)
         for key,value in sorted(vars(known).items()):
             if value is None:
                 continue
