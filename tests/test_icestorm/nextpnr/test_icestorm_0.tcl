3a49edc128265a06de55712615e79bb678f0d84a
Karol Gugala <kgugala@antmicro.com>
2020-12-22 11:16:01 +0100

    Yosys: update icestorm tests
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- tests/test_icestorm/nextpnr/test_icestorm_0.tcl
+++ tests/test_icestorm/nextpnr/test_icestorm_0.tcl
@@ -11,11 +11,11 @@ verilog_defaults -push
 verilog_defaults -add -defer
 
 verilog_defaults -add -I.
-read_verilog -sv {sv_file.sv}
-source {tcl_file.tcl}
-read_verilog {vlog_file.v}
-read_verilog {vlog05_file.v}
-read_verilog -sv {another_sv_file.sv}
+read_verilog -sv  {sv_file.sv}
+source  {tcl_file.tcl}
+read_verilog  {vlog_file.v}
+read_verilog  {vlog05_file.v}
+read_verilog -sv  {another_sv_file.sv}
 
 chparam -set vlogparam_bool 1 top_module
 chparam -set vlogparam_int 42 top_module
