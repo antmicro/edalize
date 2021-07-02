ed4cfe170aba34cc6330d2de8128821f7924b4c9
Pawel Sagan <psagan@antmicro.com>
2021-07-02 17:07:06 +0200

    test_apicula: adding test with generated uhdm configuration
    Signed-off-by: Pawel Sagan <psagan@antmicro.com>

---
+++ tests/test_apicula/uhdm/edalize_yosys_template.tcl
@@ -0,0 +1,21 @@
+yosys -import
+plugin -i uhdm 
+
+yosys -import
+source edalize_yosys_procs.tcl
+
+verilog_defaults -push
+verilog_defaults -add -defer
+
+set_defines
+set_incdirs
+read_files
+set_params
+
+verilog_defaults -pop
+
+synth $top
+
+write_blif $name.blif
+write_json $name.json
+write_edif  $name.edif
